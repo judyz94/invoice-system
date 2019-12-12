b018e076cb5fe41063ee488ebc4680734653f429
Judy Zapata <judyzh94@gmail.com>
2019-12-12 14:37:59 -0500

    Added protected accesses app menu

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -14,6 +14,16 @@ use Illuminate\Http\Response;
 class InvoiceController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @param Invoice $invoice
