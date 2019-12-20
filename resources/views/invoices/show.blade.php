aca3ff191f4af5329c67a0359d6499950f5311bd
Judy Zapata <judyzh94@gmail.com>
2019-12-19 19:27:50 -0500

    Update format

--- resources/views/invoices/show.blade.php
+++ resources/views/invoices/show.blade.php
@@ -49,9 +49,9 @@
                             <tr>
                                 <th>{{ __('Product #') }}</th>
                                 <th>{{ __('Product Name') }}</th>
-                                <th>{{ __('Price') }}</th>
+                                <th>{{ __('Unit Price') }}</th>
                                 <th>{{ __('Quantity') }}</th>
-                                <th>{{ __('Total') }}</th>
+                                <th>{{ __('Total Price') }}</th>
                                 <th>{{ __('Actions') }}</th>
                             </tr>
                             </thead>
@@ -77,47 +77,54 @@
                             @endforeach
                             </tbody>
                         </table>
-                        <div class="card-body">
-                            <div class="card-title">Add a new product to this invoice</div>
-                            <form action="{{ route('invoices.products.store', $invoice) }}" method="post">
-                                @csrf
-                                <div class="row">
-                                    <div class="form-group col-md-4">
-                                        <label for="product_id">Product</label>
-                                        <select class="custom-select" name="product_id" id="product_id" required>
-                                            <option value="">Please select a product</option>
-                                            @foreach($products as $product)
-                                                <option value="{{$product->id}}" {{ old('product_id') == $product->id ? 'selected' : ''}}>{{$product->name}}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-
-                                    <div class="form-group col-md-4">
-                                        <label for="product_price">Price</label>
-                                        <input class="form-control" type="number" id="product_price" value="{{ old('product_price') }}" placeholder="100" name="product_price" required>
-                                    </div>
-
-                                    <div class="form-group col-md-4">
-                                        <label for="product_quantity">QTY</label>
-                                        <input class="form-control" type="number" id="product_quantity" value="{{ old('product_quantity') }}" placeholder="1" name="product_quantity" required>
-                                    </div>
-                                </div>
-                                <button class="btn btn-success btn-block col-md-1" type="submit">Add</button>
-                            </form>
-                        </div>
-                        <div class="card-footer d-flex justify-content-between">
-                            <a href="{{ route('invoices.index') }}" class="btn btn-secondary">
-                                <i class="fas fa-arrow-left"></i> {{ __('Back to Invoices') }}
-                            </a>
-                            <a href="{{ route('invoiceProduct.create', [$invoice, $product]) }}" class="btn btn-success">
-                                <i class="fas fa-plus"></i>  {{ __('Add New Detail') }}
-                            </a>
-                        </div>
-
                     </div>
                 </div>
+
+                <div class="col-md-12">
+                    <div class="card-header d-flex justify-content-between">
+                        <h4><strong>Add a new product to this invoice</strong></h4>
+                    </div>
+                    <form action="{{ route('invoices.products.store', $invoice) }}" method="post">
+                        @csrf
+                        <div class="card-body">
+                        <div class="row">
+                            <div class="form-group col-md-4">
+                                <label for="product_id">Product</label>
+                                <select class="custom-select" name="product_id" id="product_id" required>
+                                    <option value="">Please select a product</option>
+                                    @foreach($products as $product)
+                                        <option value="{{$product->id}}" {{ old('product_id') == $product->id ? 'selected' : ''}}>{{$product->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group col-md-4">
+                                <label for="product_price">Price</label>
+                                <input class="form-control" type="number" id="product_price" value="{{ old('product_price') }}" placeholder="{{ __('Type a product price') }}" name="product_price" required>
+                            </div>
+
+                            <div class="form-group col-md-4">
+                                <label for="product_quantity">Quantity</label>
+                                <input class="form-control" type="number" id="product_quantity" value="{{ old('product_quantity') }}" placeholder="{{ __('Type a quantity') }} " name="product_quantity" required>
+                            </div>
+                        </div>
+                        <button class="btn btn-success btn-block col-md-1" type="submit">Add</button>
+                        </div>
+                    </form>
+                </div>
+                <br>
+                <div class="card-footer d-flex justify-content-between">
+                    <a href="{{ route('invoices.index') }}" class="btn btn-secondary">
+                        <i class="fas fa-arrow-left"></i> {{ __('Back to Invoices') }}
+                    </a>
+                    {{--<a href="{{ route('invoiceProduct.create', [$invoice, $product]) }}" class="btn btn-success">
+                        <i class="fas fa-plus"></i>  {{ __('Add New Detail') }}
+                    </a>--}}
+                </div>
+            </div>
             </div>
         </div>
+    </div>
     </div>
 @endsection
 @push('modals')
