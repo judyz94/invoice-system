1da9e66d06bdc7e99188b4ecfc2a103b5960fe5d
Judy Zapata <judyzh94@gmail.com>
2019-12-27 19:20:08 -0500

    Modified to ignore row header of excel file

--- app/Imports/InvoicesImport.php
+++ app/Imports/InvoicesImport.php
@@ -3,10 +3,12 @@
 namespace App\Imports;
 
 use App\Invoice;
-use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\{Importable, ToModel, WithHeadingRow};
 
-class InvoicesImport implements ToModel
+class InvoicesImport implements ToModel, WithHeadingRow
 {
+    use Importable;
+
     /**
     * @param array $row
     *
@@ -15,7 +17,19 @@ class InvoicesImport implements ToModel
     public function model(array $row)
     {
         return new Invoice([
-            //
+            'code' => $row['code'],
+            'expedition_date' => $row['expedition_date'],
+            'due_date' => $row['due_date'],
+            'receipt_date' => $row['receipt_date'],
+            'sale_description' => $row['sale_description'],
+            'total' => $row['total'],
+            'vat' => $row['vat'],
+            'total_with_vat' => $row['total_with_vat'],
+            'seller_id' => $row['seller_id'],
+            'customer_id' => $row['customer_id'],
+            'status' => $row['status'],
+            'user_id' => $row['user_id'],
+            'is_active' => ($row['active'] == 'YES') ? 1 : 0,
         ]);
     }
 }
