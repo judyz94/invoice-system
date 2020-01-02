419f2a1aaf3355b1732afd29c963b42c9474d07a
Judy Zapata <judyzh94@gmail.com>
2020-01-02 18:14:55 -0500

    Modified import section (title and button)

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -65,12 +65,13 @@
                         @endforeach
                         </tbody>
                     </table>
-                    <div class="card-footer d-flex justify-content-lg-start">
-
+                    <div class="card-footer justify-content-lg-start">
+                        <h5><strong>Import Excel File</strong></h5>
+                        <br>
                         <form action="{{ route('invoices.import') }}" method="post" enctype="multipart/form-data">
                             @csrf
                             <input type="file" name="file">
-                            <button class="btn btn-secondary">{{ __('Import Excel Invoices') }}</button>
+                            <button class="btn btn-secondary">{{ __('Upload') }}</button>
                         </form>
                     </div>
                 </div>
