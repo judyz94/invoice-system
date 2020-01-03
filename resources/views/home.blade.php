abcefa5bf2d51f58710c5c76c299779e77c4949c
Judy Zapata <judyzh94@gmail.com>
2020-01-03 16:21:38 -0500

    Added description of program menu options

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,21 +8,55 @@
                 <div class="card-header text-center">
                     <h1 class="card-title"><strong>{{ __('Welcome to Invoice System for PlaceToPay!') }}</strong></h1>
                 </div>
-                <div class="card-body">
+                <div class="card-body text-center m-lg-5">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-                        <div class="links text-center">
-                            <p><h5>Please choose the option from the following menu:</h5></p><br>
-                            <ul>
-                                <h3><a href="{{ route('customers.index') }}" class="text-info">Customers</a></h3><br>
-                                <h3><a href="{{ route('sellers.index') }}" class="text-info">Sellers</a></h3><br>
-                                <h3><a href="{{ route('products.index') }}" class="text-info">Products</a></h3><br>
-                                <h3><a href="{{ route('invoices.index') }}" class="text-info">Invoices</a></h3><br>
-                            </ul>
+                    <div class="card-columns">
+
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title"><i class="fas fa-users"></i> <strong>Customers</strong></h5>
+                                <p class="card-text">In this section, you can see the list of all the customers, the
+                                    detailed information of each one, create new ones, edit and delete the old ones.
+                                    In addition, you will have access to the invoices belonging to each customer.</p>
+                                <a href="{{ route('customers.index') }}" class="btn btn-primary">Go</a>
+                            </div>
                         </div>
+
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title"><i class="fas fa-users-cog"></i> <strong>Sellers</strong></h5>
+                                <p class="card-text">In this section, you can see the list of all the sellers, the
+                                    detailed information of each one, create new ones, edit and delete the old ones.
+                                    In addition, you will have access to the invoices belonging to each seller.
+                                </p>
+                                <a href="{{ route('sellers.index') }}" class="btn btn-primary">Go</a>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title"><i class="fas fa-shopping-cart"></i> <strong>Products</strong></h5>
+                                <p class="card-text">In this section, you can see the list of all the products with the
+                                    unit prices, create new ones, edit and delete the old ones.</p>
+                                <a href="{{ route('products.index') }}" class="btn btn-primary">Go</a>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title"><i class="fas fa-file-alt"></i> <strong>Invoices</strong></h5>
+                                <p class="card-text">In this section, you can see the list of all the invoices with the
+                                    detailed information of each one, create new ones, edit and delete the old ones.
+                                    In addition, you can add details(products, quantity and prices) to the invoices.
+                                    Too, you can import excel files with invoices.</p>
+                                <a href="{{ route('invoices.index') }}" class="btn btn-primary">Go</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
