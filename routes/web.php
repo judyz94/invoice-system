55b19a8e500ce560a0e162a94e08a47eca3c4ba9
judyz94 <56989936+judyz94@users.noreply.github.com>
2020-01-08 19:51:05 -0500

    Fixing update and destroy details routes

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,21 @@ Route::get('/', 'HomeController@welcome');
 Route::get('home', 'HomeController@index')->name('home');
 
 Route::resource('invoices', 'InvoiceController');
-Route::post('invoices/{invoice}/products')->uses('InvoiceController@addProduct')->name('invoices.products.store');
-
 Route::resource('customers', 'CustomerController');
 Route::resource('sellers', 'SellerController');
 Route::resource('products', 'ProductController');
-Route::resource('users', 'UserController');
 
-Route::post('/autocomplete/fetch', 'InvoiceProductController@fetch');
-Route::get('/invoices/{invoice}/products/create', 'InvoiceProductController@create')->name('invoiceProduct.create');
+Route::get('users', 'UserController@index')->name('users.index');
+Route::get('/users/{user}', 'UserController@show')->name('users.show');
+Route::delete('/users/{user}', 'UserController@destroy')->name('users.destroy');
+
+//Route::post('/autocomplete/fetch', 'InvoiceProductController@fetch');
+//Route::get('/invoices/{invoice}/products/create', 'InvoiceProductController@create')->name('invoiceProduct.create');
 //Route::post('/invoices/{invoice}/products', 'InvoiceProductController@store')->name('invoiceProduct.store');
-Route::get('/invoices/{invoice}/products/{product}/edit', 'InvoiceProductController@edit')->name('invoiceProduct.edit');
-Route::put('/invoices/{invoice}/products/{product}', 'InvoiceProductController@update')->name('invoiceProduct.update');
+Route::post('invoices/{invoice}/products')->uses('InvoiceController@addProduct')->name('invoices.products.store');
+Route::get('/invoices/{invoice}/products{product}/edit', 'InvoiceProductController@edit')->name('invoiceProduct.edit');
+
+Route::post('invoices/{invoice}/products/{product}')->uses('InvoiceController@updateProduct')->name('invoiceProduct.update');
 Route::delete('/invoices/{invoice}/products/{product}', 'InvoiceProductController@destroy')->name('invoiceProduct.destroy');
 
 Route::post('/import/invoices', 'InvoiceController@import')->name('invoices.import');
