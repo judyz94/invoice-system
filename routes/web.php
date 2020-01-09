e1f327f3b80b8169d243294745ec7c1bc4e98425
judyz94 <56989936+judyz94@users.noreply.github.com>
2020-01-09 10:15:00 -0500

    Deleted unused routes invoice details

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ Route::get('users', 'UserController@index')->name('users.index');
 Route::get('/users/{user}', 'UserController@show')->name('users.show');
 Route::delete('/users/{user}', 'UserController@destroy')->name('users.destroy');
 
-//Route::post('/autocomplete/fetch', 'InvoiceProductController@fetch');
-//Route::get('/invoices/{invoice}/products/create', 'InvoiceProductController@create')->name('invoiceProduct.create');
-//Route::post('/invoices/{invoice}/products', 'InvoiceProductController@store')->name('invoiceProduct.store');
 Route::post('invoices/{invoice}/products')->uses('InvoiceController@addProduct')->name('invoices.products.store');
 Route::get('/invoices/{invoice}/products{product}/edit', 'InvoiceProductController@edit')->name('invoiceProduct.edit');
 Route::post('invoices/{invoice}/products/{product}')->uses('InvoiceController@updateProduct')->name('invoiceProduct.update');
