4cc8405179de900b010b3d2d7416169ac3b5337b
judyz94 <56989936+judyz94@users.noreply.github.com>
2020-01-08 19:59:09 -0500

    Added button register new user

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,6 +56,9 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
+                                <a href="{{ route('register') }}" type="submit" class="btn btn-secondary">
+                                    {{ __('Register') }}
+                                </a>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
