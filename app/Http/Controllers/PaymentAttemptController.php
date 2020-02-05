173b85aed5f1130bc8a9a5469a30a8e0ae43e39f
judyz94 <56989936+judyz94@users.noreply.github.com>
2020-02-05 16:52:58 -0500

    Working redirection function

--- app/Http/Controllers/PaymentAttemptController.php
+++ app/Http/Controllers/PaymentAttemptController.php
@@ -7,13 +7,42 @@ use Illuminate\Http\Request;
 
 class PaymentAttemptController extends Controller
 {
-    public function configuration()
+    public function index()
     {
         $placetopay = new Dnetix\Redirection\PlacetoPay([
             'login' => '6dd490faf9cb87a9862245da41170ff2',
             'tranKey' => '024h1IlD',
             'url' => 'https://test.placetopay.com/reditection/',
         ]);
+
+    }
+
+    public function store()
+    {
+        $reference = $invoice->id;
+        $request = [
+        'payment' => [
+        'reference' => $invoice->id,
+        'description' => 'Testing payment',
+        'amount' => [
+            'currency' => 'USD',
+            'total' => 120,
+        ],
+    ],
+    'expiration' => date('c', strtotime('+2 days')),
+    'returnUrl' => 'http://example.com/response?reference=' . $reference,
+    'ipAddress' => '127.0.0.1',
+    'userAgent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
+        ];
+$response = $placetopay->request($request);
+if ($response->isSuccessful()) {
+    // STORE THE $response->requestId() and $response->processUrl() on your DB associated with the payment order
+    // Redirect the client to the processUrl or display it on the JS extension
+    header('Location: ' . $response->processUrl());
+} else {
+    // There was some error so check the message and log it
+    $response->status()->message();
+}
     }
 
 
