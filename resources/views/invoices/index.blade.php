e3e3bda4c793f7ebad0c1a1f310d28145b7d1083
judyz94 <56989936+judyz94@users.noreply.github.com>
2020-02-24 23:38:06 -0500

    Added conditional due_date

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -57,9 +57,11 @@
                                 <td>{{ $invoice->user->name }}</td>
                                 <td><h5>
                                     @if($invoice->status == 'New')<span class="badge badge-primary">{{ __('New') }}</span>@endif
-                                    @if($invoice->status == 'Overdue')<span class="badge badge-danger">{{ __('Overdue') }}</span>@endif
+                                        @if($invoice->due_date <= $now)
+                                            @if($invoice->status == 'Overdue')<span class="badge badge-danger">{{ __('Overdue') }}</span>@endif
+                                        @endif
                                     @if($invoice->status == 'Paid')<span class="badge badge-success">{{ __('Paid') }}</span>@endif
-                                    @if($invoice->status == 'Cancelled')<span class="badge badge-light">{{ __('Cancelled') }}</span>@endif
+                                    @if($invoice->status == 'Unpaid')<span class="badge badge-light">{{ __('Unpaid') }}</span>@endif
                                  </h5></td>
                                 <td class="text-right">
                                     <div class="btn-group btn-group-sm" role="group" aria-label="{{ __('Actions') }}">
