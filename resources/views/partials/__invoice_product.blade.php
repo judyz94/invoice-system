8bdf2bc4d51f9d95c290263dfcb52108febae9ba
judyz94 <56989936+judyz94@users.noreply.github.com>
2020-03-28 18:38:16 -0500

    Created alert on the absence of products on the invoice

---
+++ resources/views/partials/__invoice_product.blade.php
@@ -0,0 +1,20 @@
+<div class="modal fade" tabindex="-1" role="dialog" id="invoiceProduct">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"><i class="fas fa-exclamation-circle warning"></i>
+                    {{ __('The invoice has no products!') }}
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>{{ __('Please associate the required products with the invoice.') }}</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ __('Close') }}</button>
+            </div>
+        </div>
+    </div>
+</div>
