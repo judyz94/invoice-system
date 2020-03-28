27c7e2c45abc5c4e8e97ef52bde4c7fac24b4cd6
judyz94 <56989936+judyz94@users.noreply.github.com>
2020-03-28 14:35:27 -0500

    Created overdue invoice modal

---
+++ resources/views/partials/__overdue_invoice.blade.php
@@ -0,0 +1,20 @@
+<div class="modal fade" tabindex="-1" role="dialog" id="overdueInvoice">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"><i class="fas fa-exclamation-circle warning"></i>
+                    {{ __('The invoice is overdue!') }}
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>{{ __('Please contact your bank to generate a new payment rate.') }}</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ __('Close') }}</button>
+            </div>
+        </div>
+    </div>
+</div>
