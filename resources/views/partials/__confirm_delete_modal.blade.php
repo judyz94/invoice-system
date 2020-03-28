f3000b7a2629b7ffd9d05e93edac42c687545039
judyz94 <56989936+judyz94@users.noreply.github.com>
2020-03-28 14:36:07 -0500

    Added sass class

--- resources/views/partials/__confirm_delete_modal.blade.php
+++ resources/views/partials/__confirm_delete_modal.blade.php
@@ -2,7 +2,9 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">{{ __('Are you sure?') }}</h5>
+                <h5 class="modal-title"><i class="fas fa-exclamation-circle warning"></i>
+                    {{ __('Are you sure?') }}
+                </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
