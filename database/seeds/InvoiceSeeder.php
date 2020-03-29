2acde5c5226c41e39b69cf3436be1f152285e099
judyz94 <56989936+judyz94@users.noreply.github.com>
2020-03-29 13:23:18 -0500

    Modified seeders

--- database/seeds/InvoiceSeeder.php
+++ database/seeds/InvoiceSeeder.php
@@ -14,9 +14,9 @@ class InvoiceSeeder extends Seeder
     {
         DB::table('invoices')->insert([
             'code' => 'A0001',
-            'expedition_date' => '2019-11-19',
+            'expedition_date' => '2019-12-19',
             'due_date' => '2020-06-30',
-            'seller_id' => '2',
+            'seller_id' => '1',
             'sale_description' => '1 sofá y 2 pinturas',
             'customer_id' => '1',
             'vat' => '0.19',
@@ -35,15 +35,16 @@ class InvoiceSeeder extends Seeder
             'customer_id' => '2',
             'vat' => '0.19',
             'total' => '900000',
-            'total_with_vat' => '10710000',
+            'total_with_vat' => '1071000',
             'state_id' => '2',
             'user_id' => '1'
         ]);
 
         DB::table('invoices')->insert([
             'code' => 'A0026',
-            'expedition_date' => '2020-11-19',
+            'expedition_date' => '2020-03-19',
             'due_date' => '2020-06-20',
+            'receipt_date' => '2020-03-20',
             'seller_id' => '2',
             'sale_description' => '5 pacas de cuido',
             'customer_id' => '1',
@@ -51,6 +52,20 @@ class InvoiceSeeder extends Seeder
             'total' => '375000',
             'total_with_vat' => '446250',
             'state_id' => '3',
+            'user_id' => '1'
+        ]);
+
+        DB::table('invoices')->insert([
+            'code' => 'A0027',
+            'expedition_date' => '2020-03-25',
+            'due_date' => '2020-07-20',
+            'seller_id' => '1',
+            'sale_description' => '1 laptop marca Dell',
+            'customer_id' => '2',
+            'vat' => '0.19',
+            'total' => '2400000',
+            'total_with_vat' => '2856000',
+            'state_id' => '4',
             'user_id' => '1'
         ]);
     }
