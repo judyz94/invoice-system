24f158879bebfb98e2fe3011779b7d9ead40fbc4
judyz94 <judyzh94@gmail.com>
2020-04-07 18:45:14 -0500

    Oculted class user has permission

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,7 +63,7 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
         'role' => \Caffeinated\Shinobi\Middleware\UserHasRole::class,
-        'permission' => \Caffeinated\Shinobi\Middleware\UserHasPermission::class,
+        //'permission' => \Caffeinated\Shinobi\Middleware\UserHasPermission::class,
     ];
 
     /**
