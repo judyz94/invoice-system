79ca73352b5c089a5cb468690b53c1731398d702
judyz94 <judyzh94@gmail.com>
2020-04-06 21:04:28 -0500

    Created user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Http\Requests\User\UserRequest;
+use App\User;
+use Illuminate\Contracts\View\Factory;
+use Exception;
+
+class UserController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $users = User::paginate(5);
+
+        return view('users.index', compact( 'users'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(User $user)
+    {
+        return view('users.show',  compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param User $user
+     * @return Factory|\Illuminate\View\View
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit',  compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param UserRequest $request
+     * @param User $user
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(UserRequest $request, User $user)
+    {
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+
+        $user->save();
+
+        return redirect()->route('users.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param User $user
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws Exception
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return redirect()->route('users.index');
+    }
+}
+
